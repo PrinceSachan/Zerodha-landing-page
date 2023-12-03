a9a77442b478e4d71a2ac74a0ccd245a38a2b26a
PrinceSachan <prinssachan41@gmail.com>
2023-12-03 13:40:46 +0530

    Feat: added porfolio formula

--- index.js
+++ index.js
@@ -6,26 +6,31 @@ let interest = document.getElementById('interest');
 let year = document.getElementById('year');
 
 // Get the invested & returned amount element
-let invested = document.getElementById('investedAmount')
-let returnAmount = document.getElementById('returnedAmount');
+let invested = document.getElementById('investedAmount');
+let returns = document.getElementById('totalReturns')
+let returnAmount = document.getElementById('futureValue');
 
 
 function calculateInterest() {
    
     // Get the capital, interest and year input element value
-    let capitalValue = capital.value;
-    let interestValue = interest.value;
+    let principleAmount = capital.value;
+    let interestRate = interest.value / 100;
     let yearValue = year.value;
 
     // calculate simple interest
-    let simpleInterest = (capitalValue * interestValue * yearValue) / 100;
+    let futureValue = principleAmount * ((1 + interestRate) ** yearValue);
     // console.log(`simple Interest: ${simpleInterest}`);
 
-    // total amount after the interest
-    let totalamout = parseFloat(capitalValue) + simpleInterest;
+    // total amount after calculation
+    let totalamout = Math.round(futureValue)
+
+    // total retunrn in your priciple amount
+    let totalReturns = totalamout - parseFloat(principleAmount);
 
     // change the text of invested and returned amount element
-    invested.innerText = `You have Invested: ${capitalValue}$`
+    invested.innerText = `You have Invested: ${principleAmount}$`
+    returns.innerText = `You will get Total return ${totalReturns}$`
     returnAmount.innerText = `After ${yearValue} year, You will get: ${totalamout}$`
     
 }
